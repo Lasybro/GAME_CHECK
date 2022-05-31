c96a4010ca9a2c39f6a209f1140477fcb53e704f
Lasybro <99389592+Lasybro@users.noreply.github.com>
2022-05-31 18:51:42 +0700

    Add files via upload
    Play it lol

---
+++ # hi this is a game for BITTER Contained.py
@@ -0,0 +1,18 @@
+# hi this is a game for BITTER Contained 125 bot
+
+def Start():
+    ask_1 = input("Hi My Name Is [REDACTED] Who are You ?")
+
+    print("Um Well... I am Stuck In Here Contained By The [REDACTED] Company It's lonely Because I'm Here Stuck for 17 Years Now..")
+
+    print("I am Bored, It is A long Time Since I Have Seen A Staff 10-15 Years Maybe... I Do not Remember ? ")
+
+    ask_2 = input("I Wanna Play A Game '(' Yes Or No ')'  ")
+    if ask_2 == "Yes":
+        print("Yay Let's Play A ...... Hmmm.... Text Adventer Game I guess")
+    elif ask_2 == "No":
+        Close = input("Are You Sure")
+        if Close == "No":
+            print("Yay Let's Play A ...... Hmmm.... Text Adventer Game I guess")
+    
+Start()
